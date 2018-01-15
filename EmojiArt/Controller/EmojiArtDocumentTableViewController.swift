47cda4a7119235c44b8d82070854498433a0d15b
devxris <chuwei0915@gmail.com>
2018-01-15 23:01:47 +0800

    Lecture 11: UITableView

---
+++ EmojiArt/Controller/EmojiArtDocumentTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  EmojiArtDocumentTableViewController.swift
+//  EmojiArt
+//
+//  Created by Chris Huang on 15/01/2018.
+//  Copyright © 2018 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtDocumentTableViewController: UITableViewController {
+	
+	// MARK: Model
+	
+	var emojiArtDocuments = ["One", "Two", "Three"]
+	
+	// MARK: Storyboard
+	
+	@IBAction func newEmojiArt(_ sender: UIBarButtonItem) {
+		emojiArtDocuments += ["Untitled".madeUnique(withRespectTo: emojiArtDocuments)]
+		tableView.reloadData()
+	}
+
+	// MARK: UITableViewDataSource
+	
+	override func numberOfSections(in tableView: UITableView) -> Int { return 1 }
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return emojiArtDocuments.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
+		cell.textLabel?.text = emojiArtDocuments[indexPath.row]
+		return cell
+	}
+	
+	// MARK: UITableViewDelegate
+	
+	override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool { return true }
+	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			emojiArtDocuments.remove(at: indexPath.row)        // in-sync with tableView
+			tableView.deleteRows(at: [indexPath], with: .fade) // in-sync with model
+		}
+	}
+}
