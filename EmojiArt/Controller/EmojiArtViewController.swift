2ac348803acfd3dfc65db2b7d9e4582c68305edc
devxris <chuwei0915@gmail.com>
2018-01-16 16:06:57 +0800

    Lecture 12: UIScrollView - NSLayoutConstraint ( keep emojiArtView centered after zooming )

--- EmojiArt/Controller/EmojiArtViewController.swift
+++ EmojiArt/Controller/EmojiArtViewController.swift
@@ -29,6 +29,17 @@ class EmojiArtViewController: UIViewController {
 		}
 	}
 	
+	/* Keep emojiArtViewCentered automatically - make emojiArtView sticks to scrollView's contentSize
+		- when zoom too small, it's keep centered with aspect ratio
+	   	- when zoom too big, other constraints will take care because other's with higher proiority
+		1. constrain scrollView centered vertically and horizontally
+		2. constrain scrollView's width/height and outlets
+		3. set width/height to low priority
+		4. change width/height constant after first set image and after zooming ( scrollViewDidZoom delegate method )
+	*/
+	@IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+	@IBOutlet weak var scrollViewWidth: NSLayoutConstraint!
+	
 	// MARK: Properties
 	
 	var imageFetcher: ImageFetcher!
@@ -42,7 +53,9 @@ class EmojiArtViewController: UIViewController {
 			emojiArtView.backgroundImage = newValue
 			let size = newValue?.size ?? .zero
 			emojiArtView.frame = CGRect(origin: .zero, size: size)
-			scrollView.contentSize = size
+			scrollView?.contentSize = size
+			scrollViewHeight?.constant = size.height
+			scrollViewWidth?.constant = size.width
 			if let dropZone = self.dropZone, size.width > 0, size.height > 0 {
 				scrollView?.zoomScale = max(dropZone.bounds.size.width / size.width,
 										    dropZone.bounds.size.height / size.height)
@@ -54,7 +67,13 @@ class EmojiArtViewController: UIViewController {
 // MARK: UIScrollViewDelegate
 
 extension EmojiArtViewController: UIScrollViewDelegate {
+	
 	func viewForZooming(in scrollView: UIScrollView) -> UIView? { return emojiArtView }
+	
+	func scrollViewDidZoom(_ scrollView: UIScrollView) {
+		scrollViewHeight.constant = scrollView.contentSize.height
+		scrollViewWidth.constant = scrollView.contentSize.width
+	}
 }
 
 // MARK: UIDropInteractionDelegate
